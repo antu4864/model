59ae754477a7beb4ee334592cf108e318ccbb389
antu kumar <antu9649@gmail.com>
2021-08-05 18:32:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,144 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <> 
+<div className="container-">
+<div classNameName="row">    
+       
+      <div className="logo">
+       <div className="d-flex bd-highlight ">
+          <div className="p-2 w-100 bd-highlight"><h3>Logo</h3></div>
+            <div className="p-2 flex-shrink-1 bd-highlight">demo@dmail.com <br/> 55448888888</div>
+        </div>
+      </div>
+
+      <div className="menu">
+       <div className="a">
+        <a href="hshshs">Menu1</a>
+        <a href="hshshs">Menu2</a>
+        <a href="hshshs">Menu3</a>
+      
+       </div>
+     </div>
+
+     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://picsum.photos/200/300" style={{width:"100%",height:"60rem"}} alt="..."/>
     </div>
+    <div class="carousel-item">
+      <img src="https://picsum.photos/250/300" style={{width:"100%",height:"60rem"}} alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://picsum.photos/300/300" style={{width:"100%",height:"60rem"}} alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+<div className="container-fluid">
+    <div className="row">
+        <div className="col-10 mx-auto">
+<div className="row">
+<div classNameName="col-lg-4 col-md-4  ">
+<div className="image">
+  <div className="card1">
+<div class="card" style={{width: "15rem"}}>
+  <img src="https://picsum.photos/310/300" style={{width:"100%",height:"12rem"}}  alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  
+  </div>
+</div>
+</div>
+
+<div className="card1">
+<div class="card" style={{width: "15rem"}}>
+  <img src="https://picsum.photos/320/300" style={{width:"100%",height:"12rem"}}  alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  
+  </div>
+</div>
+</div>
+
+<div className="card1">
+<div class="card" style={{width: "15rem"}}>
+  <img src="https://picsum.photos/330/300" style={{width:"100%",height:"12rem"}}  alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  
+  </div>
+</div>
+</div>
+</div>
+</div>
+</div> 
+</div>
+    </div>
+</div>
+
+
+<div className="div4 text-center">
+<button type="button" class="btn btn-outline-secondary btn-lg">Click Here</button>
+ </div>
+
+
+ 
+ <div className="div5">
+      
+      <div class="d-flex bd-highlight">
+  <div class="p-2 flex-fill bd-highlight">
+  <a href="hshshs">>>Some quick links</a><br/>
+  <a href="hshshs">>>Some quick links</a><br/>
+  <a href="hshshs">>>Some quick links</a><br/>
+  <a href="hshshs">>>Some quick links</a><br/> 
+  </div>
+  <div class="p-2 flex-fill bd-highlight">
+  <p>Address<br/> 
+  dem@gmail.com<br/> 
+   55555541125<br/> 
+   other</p>
+  </div>
+  <div class="p-2 flex-fill bd-highlight">
+  <a href="hshshs">>>social media</a><br/>
+  <a href="hshshs">>>Any things u like</a><br/>
+  <a href="hshshs">>>important  links</a><br/>
+  <a href="hshshs">>>etc</a><br/> 
+  </div>
+</div>
+</div>    
+
+
+
+ <div className="div6">
+ <div class="d-flex bd-highlight">
+  <div class="p-2 flex-fill bd-highlight">copy©text</div>
+ 
+  <div class="p-2 flex-end bd-highlight">design and develop by Antu kumar</div>
+</div>
+ </div>
+
+     
+</div>
+</div>
+
+
+    </>
   );
 }
 
